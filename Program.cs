cb9ce2e79f1e023938f1507c4dc2f8e95a6559c5
PlamenPetkov53 <68778952+PlamenPetkov53@users.noreply.github.com>
2020-07-26 16:24:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace arrRot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            
+            int numberOfReverses = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfReverses; i++)
+            {
+                int firstNumber = arr[0];
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    arr[j] = arr[j + 1];
+
+                }
+                arr[arr.Length - 1] = firstNumber;
+
+                
+            }
+            Console.WriteLine(String.Join(" ", arr));
+            
+
+
+        }
+        
+    }
+}
